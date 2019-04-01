c9994c5141c96c37f0d4298a006782ad48078712
irinaespejo <iem244@nyu.edu>
2019-03-31 23:07:47 -0400

    automatic build

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker login -u "$DOCKERUSER" -p "$DOCKERPASS"
+echo "$DOCKERPASS" | docker login -u "$DOCKERUSER" --password-stdin
 docker build -f docker/docker-madminer-physics/Dockerfile -t madminertool/docker-madminer-physics .
 docker push madminertool/docker-madminer-physics
 docker build -f docker/docker-madminer-ml/Dockerfile -t madminertool/docker-madminer-ml .
