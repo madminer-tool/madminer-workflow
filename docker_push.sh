2b856ecf42bcc08868555301b5a481a5d4a16794
Sinclert Pérez <Sinclert@hotmail.com>
2020-04-14 12:32:57 +0200

    Versionate Docker images

--- docker_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "$DOCKERPASS" | docker login -u "$DOCKERUSER" --password-stdin
-docker build -f docker/docker-madminer-physics/Dockerfile -t madminertool/docker-madminer-physics .
-docker push madminertool/docker-madminer-physics
-docker build -f docker/docker-madminer-ml/Dockerfile -t madminertool/docker-madminer-ml .
-docker push madminertool/docker-madminer-ml
