41e99398ca089d0c36d9d157e8f55a9348296d8a
Sinclert Pérez <Sinclert@hotmail.com>
2020-04-15 18:10:14 +0200

    Refactor physics code (delphes.py)

--- docker-images/docker-madminer-physics/code/delphes.py
+++ docker-images/docker-madminer-physics/code/delphes.py
@@ -1,82 +1,100 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+#!/usr/bin/python
 
-import numpy as np
-#import matplotlib
-#from matplotlib import pyplot as plt
-#%matplotlib inline
-import sys 
-import yaml
-import six
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import os
-import logging
-from collections import OrderedDict
-
-from madminer.core import MadMiner
-from madminer.delphes import DelphesReader
-from madminer.sampling import combine_and_shuffle
-
-from madminer.utils.interfaces.madminer_hdf5 import (
-    save_events_to_madminer_file,
-    load_madminer_settings,
-    save_nuisance_setup_to_madminer_file,
-)
-from madminer.utils.interfaces.delphes import run_delphes
-from madminer.utils.interfaces.delphes_root import parse_delphes_root_file
-from madminer.utils.interfaces.hepmc import extract_weight_order
-from madminer.utils.interfaces.lhe import parse_lhe_file, extract_nuisance_parameters_from_lhe_file
-
-logger = logging.getLogger(__name__)
+import sys
+import yaml
+from madminer import DelphesReader
+from pathlib import Path
 
 
-h5_file = sys.argv[1]
+##########################
+#### Global variables ####
+##########################
+
+project_dir = Path(__file__).parent.parent
+
+data_dir = str(project_dir.joinpath('data'))
+card_dir = str(project_dir.joinpath('code', 'cards'))
+logs_dir = str(project_dir.joinpath('code', 'logs'))
+madg_dir = str(project_dir.joinpath('software', 'MG5_aMC_v2_6_7'))
+
+
+########################
+### Argument parsing ###
+########################
+
+config_file = sys.argv[1]
 event_path = sys.argv[2]
 input_file = sys.argv[3]
 benchmark_file = sys.argv[4]
 
-mg_dir = '/madminer/software/MG5_aMC_v2_6_2'
+with open(input_file, 'r') as f:
+    spec = yaml.safe_load(f)
 
-dp = DelphesReader(h5_file)
+with open(benchmark_file, 'r') as f:
+    benchmark = f.read()
 
-#### get benchmark name of the job
-file = open(benchmark_file,'r')
-benchmark=str(file.read())
-print(benchmark)
-print(type(benchmark))
-####
 
-dp.add_sample(
-    lhe_filename=event_path + '/unweighted_events.lhe.gz', #'/madminer/code/mg_processes/signal/Events/run_01/unweighted_events.lhe.gz'
-    hepmc_filename=event_path + '/tag_1_pythia8_events.hepmc.gz', #'/madminer/code/mg_processes/signal/Events/run_01/tag_1_pythia8_events.hepmc.gz'
-    is_background=False,
-    sampled_from_benchmark=benchmark,#'sm',
-    weights='lhe'
+########################
+## Load configuration ##
+########################
+
+reader = DelphesReader(config_file)
+
+
+#########################
+###### Run Delphes ######
+#########################
+
+reader.add_sample(
+    lhe_filename=event_path + '/unweighted_events.lhe.gz',
+    hepmc_filename=event_path + '/tag_1_pythia8_events.hepmc.gz',
+    sampled_from_benchmark=benchmark,  # 'sm'
+    weights='lhe',
 )
 
-dp.run_delphes(
-    delphes_directory=mg_dir + '/Delphes',
-    delphes_card='/madminer/code/cards/delphes_card.dat',
-    log_file='/madminer/log_delphes.log')
-    #initial_command='source activate python2'
+reader.run_delphes(
+    delphes_directory=madg_dir + '/' + 'Delphes',
+    delphes_card=card_dir + '/' + 'delphes_card.dat',
+    log_file=logs_dir + '/' + 'log_delphes.log',
+)
 
 
+############################
+## Add observables / cuts ##
+############################
 
-########### add observables and cuts from input file
-with open(input_file) as f:
-    # use safe_load instead load
-    dict_all = yaml.safe_load(f)
+for observable in spec['observables']:
+    reader.add_observable(
+        observable['name'],
+        observable['definition'],
+        observable['required'],
+        observable['default'],
+    )
 
-for observable in dict_all['observables']:
-	dp.add_observable(
-    observable['name'],
-    observable['definition'],
-    required=observable['required'],
-    default=observable['default']
-	)
+for cut in spec['cuts']:
+    reader.add_cut(cut['expression'])
 
-for cut in dict_all['cuts']:
-    dp.add_cut(cut['expression'])
-#####################################
 
-dp.analyse_delphes_samples()#reference_benchmark=benchmark
+############################
+###### Analyse events ######
+############################
 
-dp.save("/madminer/data/madminer_example_with_data_"+str(benchmark)+".h5")
+reader.analyse_delphes_samples()
+
+
+############################
+##### Save events data #####
+############################
+
+os.makedirs(data_dir, exist_ok=True)
+
+data_file_name = f'madminer_delphes_data_{benchmark}.h5'
+data_file_path = data_dir + '/' + data_file_name
+
+reader.save(data_file_path)
