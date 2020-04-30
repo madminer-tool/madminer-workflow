830ac7a56fe538cbf7cbafdde1cc2522b704f6cf
Sinclert Pérez <Sinclert@hotmail.com>
2020-04-30 11:23:23 +0200

    Fix 'combine.py' output file path

--- docker-images/docker-madminer-physics/code/combine.py
+++ docker-images/docker-madminer-physics/code/combine.py
@@ -5,10 +5,20 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import numpy as np
 import sys
-import h5py
-import shutil
+
+from madminer import combine_and_shuffle
+from pathlib import Path
+
+
+##########################
+#### Global variables ####
+##########################
+
+project_dir = Path(__file__).parent.parent
+
+data_dir = project_dir.joinpath('data')
+file_path = data_dir.joinpath("combined_delphes.h5")
 
 
 ##########################
@@ -25,24 +35,14 @@ h5_list = sys.argv[1]
 h5_list = h5_list.replace('[', '')
 h5_list = h5_list.replace(']', '')
 h5_list = h5_list.split()
-h5_list = [str(file[1:]) for file in h5_list]
-
-print(f"Cleaning... {h5_list}")
+h5_list = [str(file) for file in h5_list]
 
 
 ##########################
 #### Merging entities ####
 ##########################
 
-FINAL_NAME = "combined_delphes.h5"
-MERGE_ON = ["samples/observations", "samples/weights"]
-
-shutil.copy(h5_list[0], FINAL_NAME)
-target_file = h5py.File(FINAL_NAME, 'r+')
-
-for merge_name in MERGE_ON:
-	data_list = [np.array(h5py.File(name, 'r')[merge_name]) for name in h5_list]
-	merged_data = np.concatenate(data_list, axis=0)
-
-	del target_file[merge_name]
-	target_file.create_dataset(merge_name, data=merged_data)
+combine_and_shuffle(
+	input_filenames=h5_list,
+	output_filename=file_path,
+)
