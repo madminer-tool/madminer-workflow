ae36bf31b88de0b1366416ef469a6042eee79653
Sinclert Pérez <Sinclert@hotmail.com>
2020-06-05 15:56:32 +0200

    Delete __future__ imports on physics

--- docker-images/docker-madminer-physics/code/combine.py
+++ docker-images/docker-madminer-physics/code/combine.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import sys
-
 from madminer import combine_and_shuffle
 from pathlib import Path
 
--- docker-images/docker-madminer-physics/code/generate.py
+++ docker-images/docker-madminer-physics/code/generate.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import sys
 from madminer import MadMiner
 from pathlib import Path
